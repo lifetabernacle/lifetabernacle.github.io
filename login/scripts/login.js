daf5fd72c6c526822cc741ee938cb00cb609b3c6
Giovanni <giogallardo117@gmail.com>
2019-06-05 19:04:57 -0700

    Fixed redirect

--- login/scripts/login.js
+++ login/scripts/login.js
@@ -3,7 +3,7 @@ function check(form) {
     password = new Array("admin1", "lifetabaccess");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]) {
-		location.replace('/old/pase-events.html') /* The Administrator homepage */
+		location.replace('/old/past-events.html') /* The Administrator homepage */
 	}
 	else if(form.userid.value == username[1] && form.pswrd.value == password[1]) {
 		location.replace('/old/past-events.html') /* The members homepage */
